94e919740a56988a2f7c5b56025d36e84dae3f18
hhzl <hannes.hirzel@gmail.com>
2016-11-12 18:40:44 +0100

    fixes #32

--- spec/learnwords2/LWdbSpec.js
+++ spec/learnwords2/LWdbSpec.js
@@ -1,64 +1,6 @@
 var LWdb = require('../../src/LWdb');
 var BoxOfQuestions = require('../../src/BoxOfQuestions');
 
-// test data
-var wordList = [
-  {
-    "word": "apple",
-    "translate": "der Apfel"
-  },
-  {
-    "word": "pear",
-    "translate": "die Birne"
-  },
-  {
-    "word": "lemon",
-    "translate": "die Zitrone"
-  },
-  {
-    "word": "banana",
-    "translate": "die Banane"
-  },
-  {
-    "word": "orange",
-    "translate": "die Orange"
-  },
-  {
-    "word": "strawberry",
-    "translate": "die Erdbeere"
-  },
-  {
-    "word": "raspberry",
-    "translate": "die Himbeere"
-  },
-  {
-    "word": "blueberry",
-    "translate": "die Brombeere"
-  },
-  {
-    "word": "guava",
-    "translate": "die Guava"
-  },
-  {
-    "word": "pawpaw",
-    "translate": "die Papaya"
-  }
-];
-
-
-var aWord =   {
-    "word": "melon",
-    "translate": "die Melone"
-  };
-
-var anotherWord =
-  {
-    "word": "apricot",
-    "translate": "die Aprikose"
-  }
-;
-
-
 
 describe("LearnWords2 - App model", function() {
 
