2488ec9fbbe77f77fd0a7de6d2aefdcdc1dc3486
hhzl <hannes.hirzel@gmail.com>
2016-12-07 09:34:46 +0100

    use readline

--- scripts/demo.js
+++ scripts/demo.js
@@ -3,14 +3,30 @@ var LWdb = require('../src/LWdb');
 var BoxOfQuestions = require('../src/BoxOfQuestions');
 var wordlist = require('../data/wordlist-en-ge.js'); 
 
+var readline = require('readline');
+
+// ----------------------------------------------
+// LearnWords2 API usage
+//
+// creation:
+//     LW = new BoxOfQuestions(new LWdb('learnWords'));
+//
+// methods and properties used
+//
+//     LW.db.loadWords(wordlist)
+//     LW.question()
+//     LW.answer()
+//     LW.moveQuestionForward()
+//     LW.moveQuestionBackwards()
+//
+// ----------------------------------------------
+
+
 
 var LW = new BoxOfQuestions(new LWdb('learnWords'));
-LW.db.loadWords(wordlist);
+
+if (LW.db.numberOfWords() == 0) {LW.db.loadWords(wordlist)};
  
-
-// var aWord = {_id: 1, "word": "apple", "translate": "der Apfel"};
-
-var aWord = LW.question();
 
 console.log('\x1Bc'); 
 
@@ -20,13 +36,29 @@ console.log('Type the answers:');
 console.log('');
 
 
-console.log(aWord.word);
+// var aWord = {_id: 1, "word": "apple", "translate": "der Apfel"};
 
-process.stdin.once('data', function (data) {
-  var answer = data.toString().trim();
+var aWord = LW.question();
+console.log(aWord);
 
-  if (answer == aWord.translate) {console.log('OK')}
-  else {console.log('not OK')};
 
-  process.exit();
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
+
+
+
+rl.question(aWord.word + " = ", function(answer) {
+     answer = answer.toString().trim();
+
+     if (answer == LW.answer()) 
+		{console.log('OK'); 
+                 LW.moveQuestionForward()}
+     else {console.log('the correct answer is:', LW.answer()); 
+           LW.moveQuestionBackwards()};
+rl.close();
+
+});
+
+
