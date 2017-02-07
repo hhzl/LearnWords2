cc9e8dd2fc5f67cba81216566427367a5a3b0324
hhzl <hannes.hirzel@gmail.com>
2017-02-07 06:09:40 +0100

    fix #177

--- Grunt_config_object.js
+++
@@ -1,9 +0,0 @@
-path = require('path');
-
-var p = {"INPUT_DIR" : 'data',
-         "WEB_ROOT":  'public',
-         "BUILD_DIR" : path.join('public','js'),
-         "DIST_DIR" : 'dist'};
-
-module.exports = p;
-
