dd929cc972849e515d7fc7eed2af8945f1bb2349
hhzl <hannes.hirzel@gmail.com>
2017-02-15 13:42:24 +0100

    fix #195

--- src/data-conversion/LWjson2htmlSlides.js
+++ src/data-conversion/LWjson2htmlSlides.js
@@ -1,37 +1,70 @@
+/* LWjson2htmlSlides
+
+Input: a sequence of data objects describing words
+
+Output: HTML code for presenting slides to be used in a template
+
+*/ 
+
+
+
 var LWjson2htmlSlides = function (json) {
 	"use strict";
 
         var slides = [];
         var slideNo = 1;
+
+
+
+        function generateSlide(slides, aHTMLcontentString) {
+
+             slides.push(`<div class="slide hidden" id="slide-${slideNo}">\n`);
+             slides.push('<section class="slide-content">\n');
+
+
+             slides.push(aHTMLcontentString);
+
+
+             slides.push('</section>\n');
+             slides.push('</div>\n\n');
+        }
+
+
+
+
+
  
         json.forEach(function(element){
-            for(var key in element){
-               if(key == "picture"){
 
                   if (element.picture) {
                      slideNo += 1;
-                     slides.push(`<div class="slide hidden" id="slide-${slideNo}">\n`);
-                     slides.push('<section class="slide-content">\n');
-                     slides.push(`<img src="${element.picture}" />\n`);
-                     slides.push('</section>\n</div>\n\n');
 
+                     // show picture only
+                     generateSlide(slides,`<img src="${element.picture}" />\n`);
+
+                     
+                     // reveal letters in subsequent slides
                      for (var i=1, len=element.word.length; i<=len; i++) {
-                     slideNo += 1;
-                     slides.push(`<div class="slide hidden" id="slide-${slideNo}">\n`);
-                     slides.push('<section class="slide-content">\n');
-                     slides.push(`<img src="${element.picture}" />\n<br />`);
-                     slides.push(`<span class="bigLetters">${(element.word).substring(0,i)}</span>\n`);
-                     slides.push('</section>\n</div>\n\n');
+                         slideNo += 1;
+ 
+                         var content = `<img src="${element.picture}" />\n<br />`;
+                         content += `<span class="bigLetters">${(element.word).substring(0,i)}</span>\n`;
+
+                         generateSlide(slides,content);
                      }
+
+                     // Later TODO: add button/keyboard shortcut to add word to wordsToRepeat collection
+
                      slides.push('<!-- - - - - - - - - - - - - - - - - - - - - - - - -->\n\n');
+
                   }
-               }   
-            }  
-            }  
-        );
+
+        });
 
         return slides.join('');
 };
+
+
 
 
 module.exports = LWjson2htmlSlides;
